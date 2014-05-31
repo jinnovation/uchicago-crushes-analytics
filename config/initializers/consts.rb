7f9193da09818d30c546828b0bcd12e0ed617887
Jonathan Jin <jjin082693@gmail.com>
2014-05-31 15:34:58 -0500

    styling

--- config/initializers/consts.rb
+++ config/initializers/consts.rb
@@ -1,8 +1,13 @@
 APP_NAME = "UChicago Crushes: Analytics"
 APP_AUTHOR = "Jonathan Jin"
-APP_AUTHOR_PAGE_GITHUB = "https://github.com/jinnovation/"
 
 PAGE_NAME = "UChicagoCrushes"
 CRUSHES_NAME_DISPLAY = "UChicago Crushes"
 CRUSHES_URL  = "https://www.facebook.com/UChicagoCrushes"
 NUM_SEARCH = 100
+
+GITHUB_USERNAME = "jinnovation"
+GITHUB_PROJECT_NAME = "uchicago-crushes-analytics"
+GITHUB_URL_AUTHOR = "https://github.com/" + GITHUB_USERNAME
+GITHUB_URL_PROJECT = GITHUB_URL_AUTHOR + "/" + GITHUB_PROJECT_NAME
+
