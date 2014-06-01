36b421caecc8a4aaaee4b8690cfbc5064c047609
Jonathan Jin <jjin082693@gmail.com>
2014-06-01 15:49:25 -0500

    static page specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 describe "Static pages" do
   subject { page }
+
+  shared_examples_for "all static pages" do
+    describe "should have a header" do
+      pending
+    end
+
+    describe "should have a footer" do
+      pending
+    end
+  end
   
   describe "Home page" do
     before { visit root_path }
+
+    it_should_behave_like "all static pages"
     
     it { should have_content APP_NAME }
   end
