9209e1607db784912266580776301eb818834802
Jonathan Jin <jjin082693@gmail.com>
2014-06-01 17:31:21 -0500

    minor tweaking

--- config/initializers/consts.rb
+++ config/initializers/consts.rb
@@ -4,6 +4,11 @@ APP_AUTHOR = "Jonathan Jin"
 PAGE_NAME = "UChicagoCrushes"
 CRUSHES_NAME_DISPLAY = "UChicago Crushes"
 CRUSHES_URL  = "https://www.facebook.com/UChicagoCrushes"
+
+# FIXME: pull this from the page description
+CRUSHES_SUBMIT_URL =
+  "http://enigmatic-taiga-3279.herokuapp.com/moderators/pages/9/new/"
+
 NUM_SEARCH = 100
 
 GITHUB_USERNAME = "jinnovation"
