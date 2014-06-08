8e6ffce6da5b5bc0847548fc4ae3cdf919f83ba9
Jonathan Jin <jjin082693@gmail.com>
2014-06-07 22:01:53 -0500

    create link_to_new_tab() helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def raise_404
     raise ActionController::RoutingError.new('Not Found')
   end
+
+  def link_to_new_tab(txt, path, opts={})
+    link_to txt, path, { target: "_blank"}.merge(opts)
+  end
 end
