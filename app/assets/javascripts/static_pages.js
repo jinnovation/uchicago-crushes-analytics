142f7a80a592f9d324f03bc1f896bb2e5490d5dc
Jonathan Jin <jjin082693@gmail.com>
2014-06-08 17:41:50 -0500

    custom scrollbar styling

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,3 +1,21 @@
 $('#homepage-info-modal').on('hidden.bs.modal', function () {
     $(this).removeData('bs.modal');
 });
+
+$(function() {
+    $('.content-area').jScrollPane({
+        horizontalGutter:5,
+        verticalGutter:5,
+        'showArrows': false
+    });
+});
+
+$('.jspDrag').hide();
+$('.jspTrack').hide();
+
+$('.scrollable').hover(function(){
+    $(this).find('.jspDrag').stop(true, true).fadeIn('slow');
+    $(this).find('.jspTrack').stop(true, true).fadeIn('slow');},
+                       function(){
+                           $(this).find('.jspDrag').stop(true, true).fadeOut('slow');
+                           $(this).find('.jspTrack').stop(true, true).fadeOut('slow');});
