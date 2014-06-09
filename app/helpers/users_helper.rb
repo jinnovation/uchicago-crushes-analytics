f5e94f67b2a4a6acc23763edb420a9f684f33662
Jonathan Jin <jjin082693@gmail.com>
2014-06-09 15:02:38 -0500

    view notes and helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,26 @@
 module UsersHelper
+
+  def user_pic(user)
+    unless user.nil?
+      image_tag(user.pic_url_medium)
+    else
+      holder_tag("100x100", "Unknown")
+    end
+  end
+
+  def user_name(user)
+    if user.nil?
+      ""
+    else
+      user.full_name
+    end
+  end
+
+  def user_quotient(user)
+    if user.nil?
+      ""
+    else
+      Crush.find_by_user_id_and_post_id(user.id, post.id).quotient.to_s
+    end
+  end
 end
