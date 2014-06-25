e99e137dcbac282307711c865ea26d52b3abf7d4
Jonathan Jin <jjin@enova.com>
2014-06-25 09:29:27 -0500

    DRY-ing out Post.quotients_calc()

--- app/models/post.rb
+++ app/models/post.rb
@@ -55,6 +55,35 @@ class Post < ActiveRecord::Base
     end
   end
 
+  def quotients_set_all(users, val)
+      users.each do |user|
+        crush_curr = Crush.find_by_user_id_and_post_id(user.id, self.id)
+
+        quotient_old = crush_curr.quotient
+
+        crush_curr.update_attributes quotient: val
+        puts "Crush(#{user.full_name}, #{self.id}).quotient: #{quotient_old}"\
+        " => #{crush_curr.quotient}"
+      end    
+  end
+
+  def quotients_calc_from_num_tags(users)
+    users_tags_total = users.inject(0) do |sum,user|
+      sum += Crush.find_by_user_id_and_post_id(user.id, self.id).num_tags
+    end
+    
+    users.each do |user|
+      crush_curr = Crush.find_by_user_id_and_post_id(user.id, self.id)
+
+      quotient_old = crush_curr.quotient
+      quotient_new = (crush_curr.num_tags.to_f / users_tags_total.to_f).round 2
+
+      crush_curr.update_attributes quotient: quotient_new
+      puts "Crush(#{user.full_name}, #{self.id}).quotient: #{quotient_old}"\
+      " => #{crush_curr.quotient}"
+    end
+  end
+
   def quotients_calc
     # FIXME: what if post contains substring of {first,last,full} name
     # FIXME: account for multiple {first,last,full} name in Post.contents
@@ -76,50 +105,14 @@ class Post < ActiveRecord::Base
     if primary_user_base.any?
       puts "DISTRIB PRIMARY_USER_BASE"
 
-      primary_base_total_tags = primary_user_base.inject(0) do |sum,user|
-        sum += Crush.find_by_user_id_and_post_id(user.id, self.id).num_tags
-      end
+      quotients_calc_from_num_tags primary_user_base
 
-      primary_user_base.each do |user|
-        crush_curr = Crush.find_by_user_id_and_post_id(user.id, self.id)
-
-        quotient_old = crush_curr.quotient
-        primary_quotient_val_base = (crush_curr.num_tags.to_f / primary_base_total_tags.to_f).round 2
-
-        crush_curr.update_attributes quotient: primary_quotient_val_base
-        puts "Crush(#{user.full_name}, #{self.id}).quotient: #{quotient_old}"\
-        " => #{crush_curr.quotient}"
-      end
-
-      # FIXME: not working for every single case
-      puts "EVERY_OTHER_USER.QUOTIENT = 0"
-      other_quotient_val = 0.0
-      every_other_user.each do |user|
-        crush_curr = Crush.find_by_user_id_and_post_id(user.id, self.id)
-
-        quotient_old = crush_curr.quotient
-
-        crush_curr.update_attributes(quotient: other_quotient_val)
-        puts "Crush(#{user.full_name}, #{self.id}).quotient: #{quotient_old}"\
-        " => #{crush_curr.quotient}"
-      end
+      puts "EVERY_OTHER_USER.QUOTIENT = #{0.0}"
+      quotients_set_all every_other_user, 0.0
     else
       puts "DISTRIB TO EVERY_OTHER_USER"
 
-      other_base_total_tags = every_other_user.inject(0) do |sum,user|
-        sum += Crush.find_by_user_id_and_post_id(user.id, self.id).num_tags
-      end
-
-      every_other_user.each do |user|
-        crush_curr = Crush.find_by_user_id_and_post_id(user.id, self.id)
-
-        quotient_old = crush_curr.quotient
-        quotient_val_base = (crush_curr.num_tags.to_f / other_base_total_tags.to_f).round 2
-
-        crush_curr.update_attributes quotient: quotient_val_base
-        puts "Crush(#{user.full_name}, #{self.id}).quotient: #{quotient_old}"\
-        " => #{crush_curr.quotient}"
-      end
+      quotients_calc_from_num_tags every_other_user      
     end
   end
 
