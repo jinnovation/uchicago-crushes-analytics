0cb9f643c69da1b4287a32f4f5905ed70271295f
Jonathan Jin <jjin@enova.com>
2014-06-29 13:31:11 -0500

    post factory: add TODO

--- spec/factories/post_factory.rb
+++ spec/factories/post_factory.rb
@@ -1,5 +1,7 @@
 FactoryGirl.define do
   factory :post do
+
+    # TODO: make inherited "named_post" to give content w/ name
     content         { Faker::Lorem.paragraph 4 }
     fb_created_time { Time.now }
     fb_id           { Faker::Number.number 10 }
