9023811645e8eb88ecf9f838f242d002bb2cca41
Jonathan Jin <jjin@enova.com>
2014-07-28 15:16:19 -0500

    sidebar redirect specs

--- spec/requests/sidebar_spec.rb
+++ spec/requests/sidebar_spec.rb
@@ -7,17 +7,31 @@ describe "Sidebar" do
 
   let(:sidebar_id) { "#sidebar" }
   let(:sidebar) { find sidebar_id }
+  let(:sidebar_link_user) { sidebar.find "#sidebar-users-link" }
+  let(:sidebar_link_post) { sidebar.find "#sidebar-posts-link"}
+  let(:sidebar_link_analytics) { sidebar.find "#sidebar-analytics-link" }
 
   describe "#Users" do
     it "redirects to User#index" do
+      sidebar_link_user.click
+
+      expect(current_path).to eq users_path
     end
 
     describe "#Posts" do
-      it "redirects to Post#index"
+      it "redirects to Post#index" do
+        sidebar_link_post.click
+
+        expect(current_path).to eq posts_path
+      end
     end
 
     describe "#Analytics" do
-      it "does not redirect"
+      it "does not redirect" do
+        sidebar_link_analytics.click
+
+        expect(current_path).to eq root_path
+      end
     end
   end
 end
