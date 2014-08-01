5579221b9560acbdf76508e847b5dff65cd32a48
Jonathan Jin <jjin@enova.com>
2014-07-31 19:53:11 -0500

    post factory: add traits for content length

--- spec/factories/post_factory.rb
+++ spec/factories/post_factory.rb
@@ -6,5 +6,12 @@ FactoryGirl.define do
     fb_created_time { rand(1.year.ago..1.day.ago) }
     fb_id           { Faker::Number.number 10 }
 
+    trait :long do
+      content { Faker::Lorem.paragraph 8 }
+    end
+
+    trait :short do
+      content { Faker::Lorem.sentence 1 }
+    end
   end
 end
